f6e8330a1f1c9ba436fada42681d467a69b0045d
Reset <imstephen.1sw@gmail.com>
2025-01-20 00:01:27 -0500

    Fixed about nav link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <nav className="header">
             <div className="logo">Stephen</div>
             <ul className="nav-items">
-                <li><a href="#Home">About</a></li>
+                <li><a href="#About">About</a></li>
                 <li><a href="#tiles">Tiles</a></li>
                 <li><a href="#key-actions">Key Actions</a></li>
                 <li><a href="#harrisonburg-explorer">Harrisonburg Explorer</a></li>
